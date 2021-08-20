a6b251d553a9db97e4491eb9c9a9d725fbe7011a
Doa Bonifacio <50482180+doa-b@users.noreply.github.com>
2021-08-20 14:32:59 +0200

    Small fixes

--- src/main/java/org/jfree/date/DayDate.java
+++ src/main/java/org/jfree/date/DayDate.java
@@ -64,25 +64,6 @@ import java.text.SimpleDateFormat;
 import java.util.Calendar;
 import java.util.Date;
 
-/**
- * An abstract class that defines our requirements for manipulating dates,
- * without tying down a particular implementation.
- * <p>
- * Requirement 1 : match at least what Excel does for dates;
- * Requirement 2 : the date represented by the class is immutable;
- * <p>
- * Why not just use java.util.Date?  We will, when it makes sense.  At times,
- * java.util.Date can be *too* precise - it represents an instant in time,
- * accurate to 1/1000th of a second (with the date itself depending on the
- * time-zone).  Sometimes we just want to represent a particular day (e.g. 21
- * January 2015) without concerning ourselves about the time of day, or the
- * time-zone, or anything else.  That's what we've defined SerialDate for.
- * <p>
- * You can call getInstance() to get a concrete subclass of SerialDate,
- * without worrying about the exact implementation.
- *
- * @author David Gilbert
- */
 public abstract class DayDate implements Comparable,
         Serializable,
         MonthConstants {
@@ -732,7 +713,7 @@ public abstract class DayDate implements Comparable,
      *
      * @param d1      a boundary date for the range.
      * @param d2      the other boundary date for the range.
-     * @param include a code that controls whether or not the start and end
+     * @param interval a code that controls whether or not the start and end
      *                dates are included in the range.
      * @return A boolean.
      */
